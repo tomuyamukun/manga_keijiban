ebd61d546c34b6ceef3b35bd150b51a906eef421
tomuyamukun <3156tomochan@gmail.com>
2020-12-23 16:29:48 +0900

    n+1

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -5,7 +5,7 @@ before_action :basic_auth, only: [:new]
 
 
   def index 
-    @comics = Comic.all
+    @comics = Comic.includes(:user).all
   end
 
   def new
@@ -25,7 +25,7 @@ before_action :basic_auth, only: [:new]
 def show
   @comic = Comic.find(params[:id])
   # @comments = Comment.where(comic_id: @comic.id)
-  @comments = @comic.comments.all
+  @comments = @comic.comments.includes(:comic)
 end
 
 
